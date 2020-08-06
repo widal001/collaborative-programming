ffc2ad1845784a3dcd2e671bb79ae761a00b867b
widal001 <william.daly@mail.goucher.edu>
2020-08-06 14:50:00 -0400

    adds message sooo slow

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 def hello_world():
     messages = [
         {'user': 'bd', 'message': 'HEYO!!! WOOT!'},
-        {'user': 'jj', 'message': 'Looks good!'}
+        {'user': 'jj', 'message': 'Looks good!'},
+        {'user': 'bd', 'message': 'Sooo slow'}
     ]
     return render_template('index.html', messages=messages)
